4e77099b66fdb38d90e5354183b4067a448f575a
Joe Nace <jwnace@gmail.com>
2022-11-12 21:53:02 -0500

    added solution for day 03

--- src/aoc-2016-csharp/Program.cs
+++ src/aoc-2016-csharp/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine($"Day 01 Part 2: {Day01.Part2()}");
 
 Console.WriteLine($"Day 02 Part 1: {Day02.Part1()}");
 Console.WriteLine($"Day 02 Part 2: {Day02.Part2()}");
+
+Console.WriteLine($"Day 03 Part 1: {Day03.Part1()}");
+Console.WriteLine($"Day 03 Part 2: {Day03.Part2()}");
