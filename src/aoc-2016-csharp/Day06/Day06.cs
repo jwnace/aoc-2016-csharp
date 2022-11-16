e52a7f445030d1c60a65915ae76ad30301d83b70
Joe Nace <jwnace@gmail.com>
2022-11-15 21:25:50 -0500

    added solution for day 06

--- src/aoc-2016-csharp/Day06/Day06.cs
+++ src/aoc-2016-csharp/Day06/Day06.cs
@@ -2,7 +2,39 @@
 
 public static class Day06
 {
-    public static int Part1() => 1;
+    private static readonly string[] Input = File.ReadAllLines("Day06/day06.txt");
 
-    public static int Part2() => 2;
+    public static string Part1()
+    {
+        var result = "";
+
+        for (var i = 0; i < Input[0].Length; i++)
+        {
+            result += Input.Select(x => x[i])
+                .GroupBy(c => c)
+                .Select(g => new { g.Key, Count = g.Count() })
+                .OrderByDescending(g => g.Count)
+                .First()
+                .Key;
+        }
+
+        return result;
+    }
+
+    public static string Part2()
+    {
+        var result = "";
+
+        for (var i = 0; i < Input[0].Length; i++)
+        {
+            result += Input.Select(x => x[i])
+                .GroupBy(c => c)
+                .Select(g => new { g.Key, Count = g.Count() })
+                .OrderBy(g => g.Count)
+                .First()
+                .Key;
+        }
+
+        return result;
+    }
 }
