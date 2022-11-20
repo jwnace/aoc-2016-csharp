8c32d7729ee1f85302ba7fb814ce9b4d4cbccbec
Joe Nace <jwnace@gmail.com>
2022-11-20 00:53:52 -0500

    WIP day 11

--- src/aoc-2016-csharp/Day11/Day11.cs
+++ src/aoc-2016-csharp/Day11/Day11.cs
@@ -2,7 +2,28 @@
 
 public static class Day11
 {
-    public static int Part1() => 1;
+    private static readonly string[] Input = File.ReadAllLines("Day11/day11.txt");
 
-    public static int Part2() => 2;
+    public static int Part1()
+    {
+        var state = new string[,]
+        {
+            // A = promethium
+            // B = cobalt
+            // C = curium
+            // D = ruthenium
+            // E = plutonium
+            { "E", "AG", "AM", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " }, // first floor
+            { "E", "  ", "  ", "BG", "  ", "CG", "  ", "DG", "  ", "EG", "  " }, // second floor
+            { "E", "  ", "  ", "  ", "BM", "  ", "CM", "  ", "DM", "  ", "EM" }, // third floor
+            { "E", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " }, // fourth floor
+        };
+
+        return 1;
+    }
+
+    public static int Part2()
+    {
+        return 2;
+    }
 }
