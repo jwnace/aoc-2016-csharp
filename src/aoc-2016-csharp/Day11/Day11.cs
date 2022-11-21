3e100aff59740bc2a7986da86fa3757ee51b2c33
Joe Nace <jwnace@gmail.com>
2022-11-20 23:13:04 -0500

    WIP day 11

--- src/aoc-2016-csharp/Day11/Day11.cs
+++ src/aoc-2016-csharp/Day11/Day11.cs
@@ -3,9 +3,11 @@
 public static class Day11
 {
     private static readonly string[] Input = File.ReadAllLines("Day11/day11.txt");
+    private static readonly List<string[,]> KnownStates = new List<string[,]>();
 
     public static int Part1()
     {
+        // generate the initial state
         var state = new string[,]
         {
             // A = promethium
@@ -19,7 +21,32 @@ public static class Day11
             { "E", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  " }, // fourth floor
         };
 
+        // run until we have a result, or a set of results?
+        state = Run(state);
+
         return 1;
+    }
+
+    private static string[,]? Run(string[,] state)
+    {
+        // if this is a state we already know about, throw it out
+        if (KnownStates.Any(x => AreEquivalent(state, x)))
+        {
+            return null;
+        }
+
+        // add the current state to the list of known states
+        KnownStates.Add(state);
+
+        // TODO: try out all possible moves
+
+        throw new NotImplementedException();
+    }
+
+    private static bool AreEquivalent(string[,] a, string[,] b)
+    {
+        // TODO: implement a comparison to determine if two states are equivalent given that all pairs can be treated the same
+        throw new NotImplementedException();
     }
 
     public static int Part2()
