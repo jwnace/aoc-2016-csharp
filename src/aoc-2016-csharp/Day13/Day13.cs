03379e33ec4b93da72e1ee760f1c0aa34331bd08
Joe Nace <jwnace@gmail.com>
2022-11-21 19:39:35 -0500

    added placeholder for day 13 solution

---
+++ src/aoc-2016-csharp/Day13/Day13.cs
@@ -0,0 +1,10 @@
+namespace aoc_2016_csharp.Day13;
+
+public static class Day13
+{
+    private static readonly int Input = 1362;
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
