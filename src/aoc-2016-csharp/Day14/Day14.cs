7ce95816d3b03cf3acff2df33bc863bd4b174a64
Joe Nace <jwnace@gmail.com>
2022-11-22 08:42:46 -0500

    added placeholder for day 14 solution

---
+++ src/aoc-2016-csharp/Day14/Day14.cs
@@ -0,0 +1,10 @@
+namespace aoc_2016_csharp.Day14;
+
+public static class Day14
+{
+    private static readonly string Input = "cuanljph";
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
--- src/aoc-2016-csharp/Program.cs
+++ src/aoc-2016-csharp/Program.cs
@@ -20,5 +20,7 @@
 // Console.WriteLine($"Day 10 Part 2: {Day10.Part2()}");
 // Console.WriteLine($"Day 12 Part 1: {Day12.Part1()}");
 // Console.WriteLine($"Day 12 Part 2: {Day12.Part2()}");
-Console.WriteLine($"Day 13 Part 1: {Day13.Part1()}");
-Console.WriteLine($"Day 13 Part 2: {Day13.Part2()}");
+// Console.WriteLine($"Day 13 Part 1: {Day13.Part1()}");
+// Console.WriteLine($"Day 13 Part 2: {Day13.Part2()}");
+Console.WriteLine($"Day 14 Part 1: {Day14.Part1()}");
+Console.WriteLine($"Day 14 Part 2: {Day14.Part2()}");
