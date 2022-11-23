fddbcef1540acdb48b0152d8637ff3a75f6c4209
Joe Nace <jwnace@gmail.com>
2022-11-23 00:21:45 -0500

    working solution for day 11 part 1

--- src/aoc-2016-csharp/Day11/Day11.cs
+++ src/aoc-2016-csharp/Day11/Day11.cs
@@ -1,203 +1,154 @@
-﻿using System.Diagnostics;
-using aoc_2016_csharp.Extensions;
+﻿using aoc_2016_csharp.Extensions;
 
 namespace aoc_2016_csharp.Day11;
 
 public static class Day11
 {
-    // private static readonly string[] Input = File.ReadAllLines("Day11/day11.txt");
-    private static readonly Dictionary<List<List<string>>, int> Nodes = new();
-    private static readonly PriorityQueue<List<List<string>>, int> Queue = new();
+    private record State(string FirstFloor, string SecondFloor, string ThirdFloor, string FourthFloor, int Elevator)
+    {
+        public bool IsValid()
+        {
+            var floors = new[] { FirstFloor, SecondFloor, ThirdFloor, FourthFloor };
+            var generators = "ABCDEFG";
+
+            var q1 = floors.Any(x => x.Contains("a") && !x.Contains("A") && x.Any(y => generators.Contains(y)));
+            var q2 = floors.Any(x => x.Contains("b") && !x.Contains("B") && x.Any(y => generators.Contains(y)));
+            var q3 = floors.Any(x => x.Contains("c") && !x.Contains("C") && x.Any(y => generators.Contains(y)));
+            var q4 = floors.Any(x => x.Contains("d") && !x.Contains("D") && x.Any(y => generators.Contains(y)));
+            var q5 = floors.Any(x => x.Contains("e") && !x.Contains("E") && x.Any(y => generators.Contains(y)));
+            var q6 = floors.Any(x => x.Contains("f") && !x.Contains("F") && x.Any(y => generators.Contains(y)));
+            var q7 = floors.Any(x => x.Contains("g") && !x.Contains("G") && x.Any(y => generators.Contains(y)));
+
+            return !q1 && !q2 && !q3 && !q4 && !q5 && !q6 && !q7;
+        }
+    }
+
+    private static readonly Dictionary<State, int> Nodes = new();
+    private static readonly Queue<State> Queue = new();
 
     public static int Part1()
     {
-        // generate the initial state
-        var start = new List<List<string>>
+        var initialState = new State
+        (
+            "aA",
+            "BCDE",
+            "bcde",
+            "",
+            1
+        );
+
+        Nodes[initialState] = 0;
+        Queue.Enqueue(initialState);
+
+        while (Queue.Count > 0)
         {
-            // A = promethium, B = cobalt, C = curium, D = ruthenium, E = plutonium
-            new() { "AG", "AM", "E" }, // first floor
-            new() { "BG", "CG", "DG", "EG", }, // second floor
-            new() { "BM", "CM", "DM", "EM", }, // third floor
-            new(), // fourth floor
-        };
+            var node = Queue.Dequeue();
+            var neighbors = GetNeighbors(node);
 
-        // var start = new List<List<string>>
-        // {
-        //     // A = promethium, B = cobalt, C = curium, D = ruthenium, E = plutonium
-        //     new() { "AM", "BM", "E" }, // first floor
-        //     new() { "AG", }, // second floor
-        //     new() { "BG", }, // third floor
-        //     new(), // fourth floor
-        // };
-        
-        Queue.Enqueue(start, 0);
-        Nodes[start] = 0;
+            ProcessNeighbors(neighbors, Nodes[node] + 1);
+        }
 
-        Dijkstra();
-
-        // generate the desired state
-        var end = new List<List<string>>
-        {
-            // A = promethium, B = cobalt, C = curium, D = ruthenium, E = plutonium
-            new() { }, // first floor
-            new() { }, // second floor
-            new() { }, // third floor
-            new() { "AG", "AM", "BG", "BM", "CG", "CM", "DG", "DM", "E", "EG", "EM" }, // fourth floor
-        };
-        
-        // var end = new List<List<string>>
-        // {
-        //     // A = promethium, B = cobalt, C = curium, D = ruthenium, E = plutonium
-        //     new() { }, // first floor
-        //     new() { }, // second floor
-        //     new() { }, // third floor
-        //     new() { "AG", "AM", "BG", "BM", "E", }, // fourth floor
-        // };
-
-        return Nodes[end];
+        return Nodes.First(x => x.Key.FourthFloor.Length == 10).Value;
     }
 
     public static int Part2()
     {
-        return 2;
-    }
+        var initialState = new State
+        (
+            "AFGafg",
+            "BCDE",
+            "bcde",
+            "",
+            1
+        );
 
-    private static void Dijkstra()
-    {
-        var stopwatch = new Stopwatch();
-        stopwatch.Start();
-        var minutes = 0;
+        Nodes[initialState] = 0;
+        Queue.Enqueue(initialState);
 
         while (Queue.Count > 0)
         {
-            if (stopwatch.Elapsed.Minutes > minutes)
-            {
-                Console.WriteLine($"{stopwatch.Elapsed} => nodes: {Nodes.Count}, min: {Nodes.Values.Min()}, avg: {Nodes.Values.Average()}, max: {Nodes.Values.Max()}");
-                Console.WriteLine();
-                minutes = stopwatch.Elapsed.Minutes;
-            }
-
-            // get a node from the queue
             var node = Queue.Dequeue();
+            var neighbors = GetNeighbors(node);
 
-            // get all the possible next states
-            for (var i = 0; i < node.Count; i++)
+            ProcessNeighbors(neighbors, Nodes[node] + 1);
+        }
+
+        return Nodes.First(x => x.Key.FourthFloor.Length == 14).Value;
+    }
+
+    private static void ProcessNeighbors(IEnumerable<State> nodes, int steps)
+    {
+        foreach (var node in nodes)
+        {
+            if (Nodes.TryGetValue(node, out _) || !node.IsValid())
             {
-                var currentFloor = node[i];
-
-                if (currentFloor.Any(x => x == "E"))
-                {
-                    // the elevator is on floor i
-                    var parts = currentFloor.Where(x => x != "E")
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .Select(x => x)
-                        .ToList();
-
-                    var p1 = parts.GetCombinations(1);
-                    var p2 = parts.GetCombinations(2);
-                    var potentialLoads = p1.Concat(p2).ToList();
-
-                    var n1 = i - 1;
-                    var n2 = i + 1;
-
-                    foreach (var load in potentialLoads)
-                    {
-                        if (n2 < node.Count)
-                        {
-                            // go to n2
-                            var newState = new List<List<string>>(node.Select(x => new List<string>(x)));
-                            newState[n2].AddRange(load);
-                            newState[n2].Add("E");
-                            newState[i].RemoveAll(x => load.Contains(x));
-                            newState[i].Remove("E");
-                            newState = newState.Select(x => x.OrderBy(y => y).ToList()).ToList();
-
-                            if (newState[3].Count == 11)
-                            {
-                                throw new Exception($"FOUND THE ANSWER! IT IS: {Nodes[node] + 1}");
-                            }
-
-                            var q1 = newState.Any(x => x.Contains("AM") && !x.Contains("AG") && x.Any(y => y.Contains('G')));
-                            var q2 = newState.Any(x => x.Contains("BM") && !x.Contains("BG") && x.Any(y => y.Contains('G')));
-                            var q3 = newState.Any(x => x.Contains("CM") && !x.Contains("CG") && x.Any(y => y.Contains('G')));
-                            var q4 = newState.Any(x => x.Contains("DM") && !x.Contains("DG") && x.Any(y => y.Contains('G')));
-                            var q5 = newState.Any(x => x.Contains("EM") && !x.Contains("EG") && x.Any(y => y.Contains('G')));
-
-                            if (!q1 && !q2 && !q3 && !q4 && !q5)
-                            {
-                                var d = Nodes[node] + 1;
-
-                                if (!Nodes.TryGetValue(newState, out var nd) || d < nd)
-                                {
-                                    Nodes[newState] = d;
-                                    Queue.Enqueue(newState, d);
-                                }
-                            }
-                        }
-
-                        if (i != 1 || node[0].Count != 0)
-                        {
-                            if (i != 2 || node[0].Count != 0 || node[1].Count != 0)
-                            {
-                                if (n1 >= 0)
-                                {
-                                    var newState = new List<List<string>>(node.Select(x => new List<string>(x)));
-                                    newState[n1].AddRange(load);
-                                    newState[n1].Add("E");
-                                    newState[i].RemoveAll(x => load.Contains(x));
-                                    newState[i].Remove("E");
-                                    newState = newState.Select(x => x.OrderBy(y => y).ToList()).ToList();
-
-                                    var q1 = newState.Any(x => x.Contains("AM") && !x.Contains("AG") && x.Any(y => y.Contains('G')));
-                                    var q2 = newState.Any(x => x.Contains("BM") && !x.Contains("BG") && x.Any(y => y.Contains('G')));
-                                    var q3 = newState.Any(x => x.Contains("CM") && !x.Contains("CG") && x.Any(y => y.Contains('G')));
-                                    var q4 = newState.Any(x => x.Contains("DM") && !x.Contains("DG") && x.Any(y => y.Contains('G')));
-                                    var q5 = newState.Any(x => x.Contains("EM") && !x.Contains("EG") && x.Any(y => y.Contains('G')));
-
-                                    if (!q1 && !q2 && !q3 && !q4 && !q5)
-                                    {
-                                        var d = Nodes[node] + 1;
-
-                                        if (!Nodes.TryGetValue(newState, out var nd) || d < nd)
-                                        {
-                                            Nodes[newState] = d;
-                                            Queue.Enqueue(newState, d);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                continue;
             }
+
+            Nodes[node] = steps;
+            Queue.Enqueue(node);
         }
     }
 
-    private static bool AreStatesEquivalent(List<List<string>> a, List<List<string>> b)
+    private static IEnumerable<State> GetNeighbors(State node)
     {
-        for (int i = 0; i < 4; i++)
-        {
-            // if they have a different number of items per floor, return false
-            if (a[i].Count != b[i].Count)
-            {
-                return false;
-            }
+        var result = new List<State>();
 
-            // if they have a different number of generators per floor, return false
-            if (a[i].Count(x => x.Contains('G')) != b[i].Count(x => x.Contains('G')))
-            {
-                return false;
-            }
-            
-            // if they have a different number of microchips per floor, return false
-            if (a[i].Count(x => x.Contains('M')) != b[i].Count(x => x.Contains('M')))
-            {
-                return false;
-            }
-            
-            // TODO: what criteria need to be checked for equivalency?
+        // figure out which floor the elevator is on
+        var currentFloor = node.Elevator;
+
+        var currentFloorString = currentFloor switch
+        {
+            1 => node.FirstFloor,
+            2 => node.SecondFloor,
+            3 => node.ThirdFloor,
+            4 => node.FourthFloor,
+            _ => throw new InvalidOperationException()
+        };
+
+        var potentialLoads = currentFloorString.GetCombinations(2).Concat(currentFloorString.GetCombinations(1));
+
+        // move the elevator up if possible
+        if (currentFloor < 4)
+        {
+            // build a new state, set its distance, and add it to the queue
+            var newStates = GetNewStates(node, currentFloor + 1, potentialLoads);
+            result.AddRange(newStates);
         }
 
-        return true;
+        // move the elevator down
+        if (currentFloor > 1)
+        {
+            // build a new state, set its distance, and add it to the queue
+            var newStates = GetNewStates(node, currentFloor - 1, potentialLoads);
+            result.AddRange(newStates);
+        }
+
+        return result;
+    }
+
+    private static IEnumerable<State> GetNewStates(State node, int newFloor, IEnumerable<IEnumerable<char>> potentialLoads)
+    {
+        var result = new List<State>();
+
+        foreach (var load in potentialLoads)
+        {
+            var floors = new[] { null, node.FirstFloor, node.SecondFloor, node.ThirdFloor, node.FourthFloor };
+            var query1 = floors.Select(x => new string(x?.Except(load).ToArray()));
+            var query2 = query1.Select((x, i) => i == newFloor ? new string(x?.Concat(load).OrderBy(c => c).ToArray()) : x).ToArray();
+
+            var newState = node with
+            {
+                Elevator = newFloor,
+                FirstFloor = query2[1],
+                SecondFloor = query2[2],
+                ThirdFloor = query2[3],
+                FourthFloor = query2[4],
+            };
+
+            result.Add(newState);
+        }
+
+        return result;
     }
 }
