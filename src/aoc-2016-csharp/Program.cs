ce06e019e4725e4dd60f7aea27c6aa42e6e18e35
Joe Nace <jwnace@gmail.com>
2022-11-23 12:35:02 -0500

    removed stopwatch

--- src/aoc-2016-csharp/Program.cs
+++ src/aoc-2016-csharp/Program.cs
@@ -1,183 +1,30 @@
-﻿using System.Diagnostics;
-
-var stopwatch = new Stopwatch();
-
-stopwatch.Start();
-Console.WriteLine($"Day 01 Part 1: {Day01.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 01 Part 2: {Day01.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 02 Part 1: {Day02.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 02 Part 2: {Day02.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 03 Part 1: {Day03.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 03 Part 2: {Day03.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 04 Part 1: {Day04.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 04 Part 2: {Day04.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 05 Part 1: {Day05.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 05 Part 2: {Day05.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 06 Part 1: {Day06.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 06 Part 2: {Day06.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 07 Part 1: {Day07.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 07 Part 2: {Day07.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 08 Part 1: {Day08.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 08 Part 2: {Day08.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 09 Part 1: {Day09.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 09 Part 2: {Day09.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 10 Part 1: {Day10.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 10 Part 2: {Day10.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
+﻿// Console.WriteLine($"Day 01 Part 1: {Day01.Part1()}");
+// Console.WriteLine($"Day 01 Part 2: {Day01.Part2()}");
+// Console.WriteLine($"Day 02 Part 1: {Day02.Part1()}");
+// Console.WriteLine($"Day 02 Part 2: {Day02.Part2()}");
+// Console.WriteLine($"Day 03 Part 1: {Day03.Part1()}");
+// Console.WriteLine($"Day 03 Part 2: {Day03.Part2()}");
+// Console.WriteLine($"Day 04 Part 1: {Day04.Part1()}");
+// Console.WriteLine($"Day 04 Part 2: {Day04.Part2()}");
+// Console.WriteLine($"Day 05 Part 1: {Day05.Part1()}");
+// Console.WriteLine($"Day 05 Part 2: {Day05.Part2()}");
+// Console.WriteLine($"Day 06 Part 1: {Day06.Part1()}");
+// Console.WriteLine($"Day 06 Part 2: {Day06.Part2()}");
+// Console.WriteLine($"Day 07 Part 1: {Day07.Part1()}");
+// Console.WriteLine($"Day 07 Part 2: {Day07.Part2()}");
+// Console.WriteLine($"Day 08 Part 1: {Day08.Part1()}");
+// Console.WriteLine($"Day 08 Part 2: {Day08.Part2()}");
+// Console.WriteLine($"Day 09 Part 1: {Day09.Part1()}");
+// Console.WriteLine($"Day 09 Part 2: {Day09.Part2()}");
+// Console.WriteLine($"Day 10 Part 1: {Day10.Part1()}");
+// Console.WriteLine($"Day 10 Part 2: {Day10.Part2()}");
 Console.WriteLine($"Day 11 Part 1: {Day11.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
 Console.WriteLine($"Day 11 Part 2: {Day11.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 12 Part 1: {Day12.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 12 Part 2: {Day12.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 13 Part 1: {Day13.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 13 Part 2: {Day13.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 14 Part 1: {Day14.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 14 Part 2: {Day14.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 15 Part 1: {Day15.Part1()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
-
-stopwatch.Start();
-Console.WriteLine($"Day 15 Part 2: {Day15.Part2()}");
-stopwatch.Stop();
-Console.WriteLine($"Took {stopwatch.Elapsed} to run!");
-stopwatch.Reset();
+// Console.WriteLine($"Day 12 Part 1: {Day12.Part1()}");
+// Console.WriteLine($"Day 12 Part 2: {Day12.Part2()}");
+// Console.WriteLine($"Day 13 Part 1: {Day13.Part1()}");
+// Console.WriteLine($"Day 13 Part 2: {Day13.Part2()}");
+// Console.WriteLine($"Day 14 Part 1: {Day14.Part1()}");
+// Console.WriteLine($"Day 14 Part 2: {Day14.Part2()}");
+// Console.WriteLine($"Day 15 Part 1: {Day15.Part1()}");
+// Console.WriteLine($"Day 15 Part 2: {Day15.Part2()}");
