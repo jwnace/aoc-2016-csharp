1d80066e528b03d6397def85ed77ead250c73957
Joe Nace <jwnace@gmail.com>
2022-11-25 00:19:22 -0500

    added placeholder for day 19 solution

---
+++ src/aoc-2016-csharp/Day19/Day19.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc_2016_csharp.Day19;
+
+public static class Day19
+{
+    public static string Input = File.ReadAllText("Day19/day19.txt");
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
