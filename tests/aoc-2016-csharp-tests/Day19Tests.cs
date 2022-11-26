456c5f2fa584fab6d9ccd0bc2db434cc4958aa3e
Joe Nace <jwnace@gmail.com>
2022-11-26 13:18:45 -0500

    added test cases for my real input

--- tests/aoc-2016-csharp-tests/Day19Tests.cs
+++ tests/aoc-2016-csharp-tests/Day19Tests.cs
@@ -6,6 +6,7 @@ namespace aoc_2016_csharp_tests;
 public class Day19Tests
 {
     [TestCase(5, 3)]
+    [TestCase(3004953, 1815603)]
     public void SolvePart1Test(int input, int expected)
     {
         // act
@@ -16,6 +17,7 @@ public class Day19Tests
     }
 
     [TestCase(5, 2)]
+    [TestCase(3004953, 1410630)]
     public void SolvePart2Test(int input, int expected)
     {
         // act
