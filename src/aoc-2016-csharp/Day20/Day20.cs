7bfbed687a41d57e60fbde9d2a16e54bf7aebdd4
Joe Nace <jwnace@gmail.com>
2022-11-26 21:10:47 -0500

    working solution for day 20 part 1, still working on part 2

--- src/aoc-2016-csharp/Day20/Day20.cs
+++ src/aoc-2016-csharp/Day20/Day20.cs
@@ -2,7 +2,30 @@
 
 public static class Day20
 {
-    public static int Part1() => 1;
+    private static readonly string[] Input = File.ReadAllLines("Day20/day20.txt");
 
-    public static int Part2() => 2;
+    public static long Part1() => GetLowestAllowedIp();
+
+    public static long Part2() => 2;
+
+    private static long GetLowestAllowedIp()
+    {
+        var ranges = Input.Select(x => (Start: long.Parse(x.Split('-')[0]), End: long.Parse(x.Split('-')[1])))
+            .OrderBy(x => x.Start)
+            .ThenBy(x => x.End)
+            .ToList();
+
+        for (var i = 0; i < ranges.Count - 1; i++)
+        {
+            var left = ranges[i];
+            var right = ranges[i + 1];
+
+            if (left.End + 1 < right.Start)
+            {
+                return left.End + 1;
+            }
+        }
+
+        return 0;
+    }
 }
