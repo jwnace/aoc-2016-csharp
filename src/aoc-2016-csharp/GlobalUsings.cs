754bc409d5404a9cb4ab3539a72e876b038edada
Joe Nace <jwnace@gmail.com>
2022-11-27 18:49:52 -0500

    added placeholder for day 24 solution

--- src/aoc-2016-csharp/GlobalUsings.cs
+++ src/aoc-2016-csharp/GlobalUsings.cs
@@ -21,3 +21,4 @@ global using aoc_2016_csharp.Day20;
 global using aoc_2016_csharp.Day21;
 global using aoc_2016_csharp.Day22;
 global using aoc_2016_csharp.Day23;
+global using aoc_2016_csharp.Day24;
